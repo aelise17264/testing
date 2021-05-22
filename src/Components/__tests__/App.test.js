c0386292120d66d4e10d5aad2be132812221eea0
unknown <aelise17264@gmail.com>
2021-05-22 17:25:33 -0600

    declared wrapped variable globally in app.test.js

--- src/Components/__tests__/App.test.js
+++ src/Components/__tests__/App.test.js
@@ -4,24 +4,23 @@ import App from 'Components/App';
 import CommentBox from 'Components/CommentBox';
 import CommentList from 'Components/CommentList';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+//declared in outer scope so it is globally accessible
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+    //wrapped has additional functionality on top of it
+
+})
 
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
-  //wrapped has additional functionality on top of it
+  
   expect(wrapped.find(CommentBox).length).toEqual(1)
   //find will return an array with the number of times CommentBox was found
   
 });
 
 it('shows the comment list', () => {
-  const wrapped = shallow(<App />);
-  //wrapped has additional functionality on top of it
   expect(wrapped.find(CommentList).length).toEqual(1)
-  //find will return an array with the number of times CommentBox was found
   
 });
