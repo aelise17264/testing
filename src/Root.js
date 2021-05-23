3f6a92ffc9183233c29dd76010d976a4848760ff
unknown <aelise17264@gmail.com>
2021-05-22 19:00:53 -0600

    added Root.js to pass props to our app & our test files

---
+++ src/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from 'Reducers';
+
+export default () =>{
+    return(
+        <Provider store={createStore(reducers, {})}>
+            {props.children} 
+        </Provider>
+    )
+}
+
+            //allows us to take the component that we created & allows us to wrap other components inside
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from 'Reducers';
+import Root from 'Root';
 
 import App from 'Components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={createStore(reducers, {})}>
+  <Root>
     <App />
-    </Provider>
+  </Root>
   </React.StrictMode>,
   document.getElementById('root')
 );
