735ed59994aa125b6538e04e05a8b1f3b5cfd2f6
unknown <aelise17264@gmail.com>
2021-05-22 18:51:10 -0600

    connected our actions and reducers to the commentbox

--- src/Components/CommentBox.js
+++ src/Components/CommentBox.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as actions from 'actions';
+
 
 class CommentBox extends Component{
     state = { comment: ""};
@@ -10,7 +13,7 @@ class CommentBox extends Component{
     handleSubmit = (event) => {
         event.preventDefault()
 
-
+        this.props.saveComment(this.state.comment)
 
         this.setState({comment: ""})
     }
@@ -30,4 +33,4 @@ class CommentBox extends Component{
     }
 }
 
-export default CommentBox
+export default connect(null, actions) (CommentBox);
