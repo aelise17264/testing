83fb23485481a043e68d4a9f1f0574d7a3face73
unknown <aelise17264@gmail.com>
2021-05-22 18:11:49 -0600

    two passing tests for commentbox

--- src/Components/__tests__/CommentBox.test.js
+++ src/Components/__tests__/CommentBox.test.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import CommentBox from 'Components/CommentBox';
-import {shallow} from 'enzyme'; //render out a single instance of a component & none of its children
+import {mount} from 'enzyme'; 
 
 let wrapped;
 
 beforeEach(() => {
-  wrapped = shallow(<CommentBox />);
-    //wrapped has additional functionality on top of it
+  wrapped = mount(<CommentBox />);
+})
 
+afterEach(() => {
+    wrapped.unmount();
 })
 
 it('has a text area and a submit button', () => {
-    console.log(wrapped.find("textarea").length);
+    //console.log(wrapped.find("textarea").length);
+    expect(wrapped.find('textarea').length).toEqual(1)
+    expect(wrapped.find('button').length).toEqual(1)
+})
+
+it('has a text area that users can type in', () => {
+    //find the textarea element
+    //submit a change event
+    // provide a fake event object
+    //force the component to update
+    //assert that the textarea's value has changed
+    wrapped.find('textarea').simulate('change', {
+        target: {value: 'new comment'}
+    });
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+
 })
