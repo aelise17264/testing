4d927e1613b0873d7890ae0162d37b4b4986ecb7
unknown <aelise17264@gmail.com>
2021-05-24 13:39:39 -0600

    tested commentlist

--- src/Components/__tests__/CommentList.test.js
+++ src/Components/__tests__/CommentList.test.js
@@ -18,6 +18,12 @@ beforeEach(() => {
 });
 
 it('creates one LI per comment', () => {
-    console.log(wrapped.find('li').length);
+    expect(wrapped.find('li').length).toEqual(2)
+});
+
+it('shows the text for each comment', () => {
+    // console.log(wrapped.render().text()) returns the visible text of the HTML
+    expect(wrapped.render().text()).toContain('Comment 1');
+    expect(wrapped.render().text()).toContain('Comment 2');
 })
 
