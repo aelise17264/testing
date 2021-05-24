eb349bb63bcc927e8355845d7a56e5580ea02418
unknown <aelise17264@gmail.com>
2021-05-24 12:51:45 -0600

    configured commentlist through redux class components, mapped state to props, list of comments now showing on app

--- src/Components/CommentList.js
+++ src/Components/CommentList.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default function CommentList() {
-    return (
-    <div>This is the comment List</div>
-    )
+class CommentList extends Component {
+
+    renderComments(){
+        return this.props.comments.map(comment => {
+            return <li key={comment}>{comment}</li>
+        })
+    }
+
+    render(){
+        return(
+            <div className="commentList">
+                <ul>
+                    {this.renderComments()}
+                </ul>
+            </div>
+        )
+    }
 }
+
+
+function mapStateToProps(state){
+    return {comments: state.comments}
+}
+
+export default connect(mapStateToProps)(CommentList)
