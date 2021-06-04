a4d26003a7d9fed6f46f0237dce859bc46cf3bb5
unknown <aelise17264@gmail.com>
2021-06-04 15:08:02 -0600

    used connect to map state to props & added appropriate sign in or sign out buttons depending on the users authentication

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import {Route, Link} from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'Components/CommentBox';
 import CommentList from 'Components/CommentList';
 
 
-export default class App extends Component {
+class App extends Component {
   renderButton(){
-
+    if(this.props.auth){
+      return(
+        <button>Sign Out</button>
+      );
+    }else{
+      return(
+        <button>Sign In</button>
+      )
+    }
   }
 
   renderHeader(){
@@ -28,6 +37,7 @@ export default class App extends Component {
   render(){
   return (
     <div className="App">
+    {this.renderHeader()}
       <Route path="/post" component={CommentBox} />
       <Route path="/" exact component={CommentList} />
     </div>
@@ -35,3 +45,9 @@ export default class App extends Component {
 }
 }
 
+
+function mapStateToProps(state){
+  return {auth: state.auth}
+}
+
+export default connect(mapStateToProps)(App)
