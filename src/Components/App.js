56f2430760746a1109035534ceededf51090ca65
unknown <aelise17264@gmail.com>
2021-06-04 14:50:13 -0600

    added git render method to app component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route, Link} from 'react-router-dom';
 import CommentBox from 'Components/CommentBox';
 import CommentList from 'Components/CommentList';
 
 
-function App() {
+export default class App extends Component {
+  renderButton(){
+
+  }
+
+  renderHeader(){
+    return(
+      <ul>
+        <li>
+          <Link to="/" >Home</Link>
+        </li>
+        <li>
+          <Link to="/post" >Post a Comment</Link>
+        </li>
+        <li>        
+          {this.renderButton()}
+        </li>
+      </ul>
+    )
+  }
+
+  render(){
   return (
     <div className="App">
       <Route path="/post" component={CommentBox} />
@@ -12,5 +33,5 @@ function App() {
     </div>
   );
 }
+}
 
-export default App;
